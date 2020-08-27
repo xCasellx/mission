0f34b618e4f7610e8fc256c5f0d7d155f8deadd6
Dminty <syrotayak@gmail.com>
2020-08-27 23:44:24 +0300

    upd bd

--- users.sql
+++ users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Авг 27 2020 г., 12:26
+-- Время создания: Авг 27 2020 г., 23:43
 -- Версия сервера: 5.5.62
 -- Версия PHP: 7.1.33
 
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- База данных: `users`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `date` varchar(50) NOT NULL,
+  `text` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `comments`
+--
+
+INSERT INTO `comments` (`id`, `user_id`, `date`, `text`) VALUES
+(17, 22, '2020-08-27 23:41:45', 'HI'),
+(18, 23, '2020-08-27 23:42:10', 'TEST'),
+(19, 23, '2020-08-27 23:42:15', 'RRRRR');
 
 -- --------------------------------------------------------
 
@@ -43,11 +65,20 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `first_name`, `second_name`, `email`, `number`, `date`, `town`, `password`) VALUES
-(21, 'Dima', 'Syrotiak', 'syrotyaka@gmail.com', '0969089358', '1999-03-04', 'Комсомольск', '41890cd2ac71e06b5f2c9ad5ccc07b45');
+(21, 'Dima', 'Syrotiak', 'syrotyaka@gmail.com', '0969089358', '1999-03-04', 'Комсомольск', '41890cd2ac71e06b5f2c9ad5ccc07b45'),
+(22, 'Vova', 'Ivanov', 'Jamser51@gmail.com', '7777777777', '2020-01-29', 'Kremenchyk', '4297f44b13955235245b2497399d7a93'),
+(23, 'Dima', 'Syrotiak', 'daggerfeed@gmail.com', '0969089358', '2020-07-30', 'Kremenchyk', '4297f44b13955235245b2497399d7a93');
 
 --
 -- Индексы сохранённых таблиц
 --
+
+--
+-- Индексы таблицы `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Индексы таблицы `user`
@@ -60,10 +91,26 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
 -- AUTO_INCREMENT для таблицы `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
